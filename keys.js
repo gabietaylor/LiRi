0a682f5d299d506106b4cb265b5ebf6be478ace6
gabietaylor <gabie.taylor@yahoo.com>
2017-06-14 17:52:10 -0400

    we are logging

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'XPC9N11U5QwaUgSriGPJQ2oJh',
+    consumer_secret: 'fCnbbM276BsYhO8AOyTQoQkPGsTmuOM2mzlrh81ecu6QVeim3O',
+    access_token_key: '875096341004910592-rpEQrLvfdAnDvgvJxSBP7pvt2P0ljsK',
+    access_token_secret: 'R74IafXyKtL3vgh6cHjp0BVIhj8vvH9QZCS85ltAudpNA',
+}
