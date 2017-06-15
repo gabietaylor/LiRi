75a85bdc0e3c5b7dc56979e2f38eb2e16facc2c0
gabietaylor <gabie.taylor@yahoo.com>
2017-06-15 18:28:33 -0400

    spotify logging

--- liri.js
+++ liri.js
@@ -1,21 +1,22 @@
-// Vars for index
+// Vars To Hold Index
 var process2 = process.argv[2];
 var process3 = process.argv[3];
 
 //-------------------------------------------Twitter---------------------------------------------------------------
+
 // Tweet Vars
 var keys = require('./keys.js');
 //console.log(keys);
 
 var Twitter = require('twitter');
 
-// Twitter user based auth.
+// Twitter User Based Auth.
 var client = new Twitter(
     keys
 );
 //console.log(client);
 
-// index is tweets
+// Indexes Tweets
 if (process2 === "tweets") {
 
     // 20 tweets
@@ -23,26 +24,35 @@ if (process2 === "tweets") {
         screen_name: 'EzioAuditoreAno'
     };
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    //console.log(tweets);
-
-    // If theres an error, it'll throw it
-    if (error) {
-        throw error;
-    }
-    // If theres isnt an error, it'll console log the tweets
-    if (!error) {
         //console.log(tweets);
-    }
-    for (var i = 0; i < tweets.length; i++) {
-        console.log(tweets[i].text);
-    }
-    for (var i = 0; i < tweets.length; i++) {
-        console.log(tweets[i].created_at);
-    }
-});
+
+        // Throw Error
+        if (error) {
+            throw error;
+        }
+        // No Error
+        if (!error) {
+            //console.log(tweets);
+        }
+        for (var i = 0; i < tweets.length; i++) {
+            console.log(tweets[i].text);
+        }
+        for (var i = 0; i < tweets.length; i++) {
+            console.log(tweets[i].created_at);
+        }
+    });
 }
+
 // ---------------------------Spotify
 
+var Spotify = require('node-spotify-api');
+
+// Spotify User Based Auth.
+var spotify = new Spotify ({
+	id: "b47ff273b32f46e2b414f10a09871472",
+	secret: "ed9a99de01e243ad971ea12452eba1ea"
+});
+console.log(spotify); 
 
 
 
